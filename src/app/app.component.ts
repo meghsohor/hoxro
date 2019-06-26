a2ef7fa54a035d504a9fe2b82e998dc47e950432
SHUVO <meghsohor@yahoo.com>
2019-06-26 17:21:21 +0600

    M

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,14 +22,40 @@ export class AppComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
+        //let pageTitle = this.router.routerState.snapshot.root.children[0].data['title'];
         //console.log(this.activatedRoute.firstChild.data.value.title);
 
         //this.titleService.setTitle(this.activatedRoute.firstChild.data.value.title);
         
         //$("html, body").animate({ scrollTop: 0 }, "slow");
+
+        const title = this.getTitle(this.router.routerState, this.router.routerState.root).join(' | ');
+        this.titleService.setTitle(title);
       }
-  });
+    });
+
+    /* this.router.events
+      .filter((event) => event instanceof NavigationEnd)
+      .map(() => this.router)
+      .subscribe((event) => {
+        const title = this.getTitle(this.router.routerState, this.router.routerState.root).join(' | ');
+        this.titleService.setTitle(title);
+      }
+      ); */
+  }
+
+  getTitle(state, parent) {
+    const data = [];
+    if (parent && parent.snapshot.data && parent.snapshot.data.title) {
+      data.push(parent.snapshot.data.title);
+    }
+
+    if (state && parent) {
+      data.push(... this.getTitle(state, state.firstChild(parent)));
+    }
+    return data;
   }
 }
